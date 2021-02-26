ce4448a8a34818db97b7e6ef821cb07c09961afe
Priya Patel <50588337+priyapatel1@users.noreply.github.com>
2021-02-26 08:15:08 -0500

    Files from CODE
    Files from the CODE folder

---
+++ CODE/VIIRS_NASA-master/VIIRS_NASA-master/PRP_tester.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 10:46:49 2020
+
+@author: patelp23
+"""
+
+import os
+path = "C:\\Users\\patelp23\\OneDrive - University of Toronto\\nasa\\prp_code"
+os.chdir(path)
+
+
+
+# import netCDF4
+from netCDF4 import Dataset
+
+
+    
---
+++ CODE/pyprac/earthdata1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 17:50:05 2020
+
+@author: priya
+"""
+
